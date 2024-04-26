3ff6f8fd362bb3d5208f15ad42c959ba2d0d1e7e
Ashwin-3cS <flashashwin2004@gmail.com>
2024-04-26 18:59:20 +0530

    socket io is just pending

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,16 +1,35 @@
 import { BsSend } from "react-icons/bs";
+import useSendMessage from "../../hooks/useSendMessage";
+import { useState } from "react";
 
 const MessageInput = () => {
-	return (
-		<form className='px-4 my-3'>
+
+	const [message,setMessage ] = useState("");
+	const {loading,sendMessage} = useSendMessage();
+
+	const handleSubmit = async(e)=>{
+		e.preventDefault();
+		if(!message) return; //no message return it dont run this function
+		await sendMessage(message);
+		setMessage("");
+	}
+
+	const handleChange= (e)=>{
+		setMessage(e.target.value);
+
+	}
+	return (	
+		<form className='px-4 my-3' onSubmit={handleSubmit}>
 			<div className='w-full relative'>
 				<input
 					type='text'
 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
 					placeholder='Send a message'
+					value={message}
+					onChange={handleChange}
 				/>
 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-					<BsSend />
+					{loading ? <div className="loading loading-spinner mx-auto"></div> : <BsSend/>}
 				</button>
 			</div>
 		</form>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,25 +1,60 @@
+import { useState } from "react";
 import GenderCheckbox from "./GenderCheckBox"
+import { Link } from 'react-router-dom';
+import useSignup from "../../hooks/useSignup";
+
+
+
 const SignUp = () => {
+
+	const [inputs,setInputs] = useState({
+		fullName: '',
+		userName: '',
+		password: '',
+		confirmPassword: '',
+		gender: ''	
+	})
+
+	const {loading,signup} = useSignup()
+
+	const handleCheckboxChange = (gender) => {
+		setInputs({...inputs,gender})	
+	}
+
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await signup(inputs)
+	}
+
+
+
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up <span className='text-amber-700'> FlyApp</span>
+					SignUp <span className='text-amber-700'> HiFly</span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Full Name</span>
 						</label>
-						<input type='text' placeholder='Ash Win' className='w-full input input-bordered  h-10' />
+						<input type='text' placeholder='Ash Win' className='w-full input input-bordered  h-10'
+							value = {inputs.fullName}
+							onChange={(e)=>setInputs({...inputs,fullName:e.target.value})} 
+						/>
 					</div>
 
 					<div>
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='ashWin' className='w-full input input-bordered h-10' />
+						<input type='text' placeholder='ashWin' className='w-full input input-bordered h-10'
+							onChange={(e)=>setInputs({...inputs,userName:e.target.value})} 
+ 
+						/>
 					</div>
 
 					<div>
@@ -30,6 +65,8 @@ const SignUp = () => {
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
+							onChange={(e)=>setInputs({...inputs,password:e.target.value})} 
+
 						/>
 					</div>
 
@@ -41,17 +78,20 @@ const SignUp = () => {
 							type='password'
 							placeholder='Confirm Password'
 							className='w-full input input-bordered h-10'
+							onChange={(e)=>setInputs({...inputs,confirmPassword:e.target.value})} 				
 						/>
 					</div>
 
-					<GenderCheckbox />
+					<GenderCheckbox  onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender}/>
 
-					<a className='text-sm hover:underline hover:text-amber-700 mt-2 inline-block' href='#'>
+					<Link  to={"/login"} className='text-sm hover:underline hover:text-amber-700 mt-2 inline-block' >
 						Already have an account?
-					</a>
+					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700 ' disabled={loading} >
+							{loading ? <span className="loading loading-spinner"></span>: "Sign Up"}
+						</button> 
 					</div>
 				</form>
 			</div>
