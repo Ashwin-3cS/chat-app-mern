7c8864ab3b36cbfabca221c8266226bc5b396bca
Ashwin-3cS <flashashwin2004@gmail.com>
2024-04-26 21:15:08 +0530

    commit before deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 
 
 import authRoutes from "./routes/auth.routes.js"
@@ -12,8 +14,11 @@ import { app, server } from "./socket/socket.js";
 
 
 
+
 dotenv.config();
 const PORT = process.env.PORT || 5000 ;
+
+const __dirname = path.resolve();
 
 
 // app.get ("/", (req,res)=>{
@@ -26,6 +31,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 
 server.listen(PORT , (req,res) => {
