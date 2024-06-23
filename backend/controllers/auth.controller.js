75ce416fa7f72ecbd1b4a6d2364786c827248e65
Ashwin-3cS <flashashwin2004@gmail.com>
2024-06-23 14:30:51 +0530

    made a change in controlers backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -8,7 +8,8 @@ export const signup = async (req,res) =>{
         
         if(password!==confirmPassword){
 
-                  return res.status(400).json({error:"Passwords don't match"})
+            return res.status(400).json({error:"Passwords don't match"})
+
         }
 
         const user = await User.findOne({userName});
